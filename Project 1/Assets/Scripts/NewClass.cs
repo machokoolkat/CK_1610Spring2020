ad4a669bf0ff72e2535ff9e3afd3d4d19a2a5ce0
c <machokoolkat@gmail.com>
2020-01-13 11:18:14 -0700

    added NewClass

---
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello World");
+    }
+}
