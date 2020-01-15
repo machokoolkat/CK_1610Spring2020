0735c8a554b49a3f00b3a9b8e745d169c11d0721
C <machokoolkat@gmail.com>
2020-01-15 10:53:28 -0700

    Script Syntax and Variables
    Reviewed scripts and learned about variables and their syntax. Added int, float, and GameObject

--- Project 1/Assets/Scripts/NewClass.cs
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class NewClass : MonoBehaviour
 {
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
     // Start is called before the first frame update
     private void Start()
     {
-        print("Hello World");
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        print("Hello World");
-    }
 }
