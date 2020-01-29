3ee40eb75884bb81be57061f1a4811862520f9ad
c <machokoolkat@gmail.com>
2020-01-29 11:02:59 -0700

    Mouse Events
    Created all 6 mouse events and invoked them, not applied to anything yet

---
+++ Project 1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseDragEvent, mouseOverEvent, mouseUpEvent, mouseEnterEvent, mouseExitEvent;
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+}
