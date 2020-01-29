74b2c960a78d64a0ea0eec70f02cd7d6e583fb59
c <machokoolkat@gmail.com>
2020-01-29 10:28:07 -0700

    Class Starter
    Created new Triggers, exit, enter, and stay. applied it to a new cube.

---
+++ Project 1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public float triggeringFloat;
+    public int triggeringScore;
+    public string triggeringString;
+    public UnityEvent triggerFloat;
+    public UnityEvent triggerScore;
+    public UnityEvent triggerString;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerFloat.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerScore.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerString.Invoke();
+    }
+}
