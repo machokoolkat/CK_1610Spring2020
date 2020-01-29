c6fd2c9bd268ef824f4cc8c6ba17e4d90a76a943
c <machokoolkat@gmail.com>
2020-01-28 23:56:15 -0700

    Homework
    wrote the basic code for OnTriggerStay and OnTriggerExit

---
+++ Project 1/Assets/Scripts/TriggerEventStay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventStay : MonoBehaviour
+{
+    public UnityEvent triggerEventStay;
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerEventStay.Invoke();
+    }
+}
---
+++ Project 1/Assets/Scripts/TriggertEventExit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggertEventExit : MonoBehaviour
+{
+    public UnityEvent triggerEventExit;
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerEventExit.Invoke();
+    }
+}
