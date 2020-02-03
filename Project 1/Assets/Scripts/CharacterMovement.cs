fa43e3925166e8d588b52e428a915b6f17e36466
c <machokoolkat@gmail.com>
2020-02-03 11:21:43 -0700

    Movement Scripts
    Started testing character movements, horizontal and jumps.

---
+++ Project 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Vertical")*speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
