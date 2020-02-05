ba80d2199d20c80ef30df8475640321f6ac83ad5
c <machokoolkat@gmail.com>
2020-02-05 11:13:53 -0700

    Review Movement Scripts
    Same as last time, added second character with movements.

---
+++ Project 1/Assets/Scripts/CharacterTwoMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CharacterTwoMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 10f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+
+        controller.Move(positionDirection*Time.deltaTime);
+        positionDirection.y += gravity;
+    }
+}
