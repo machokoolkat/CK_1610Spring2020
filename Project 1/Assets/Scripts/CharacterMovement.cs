d67c159beffbfeda05bdcb2391678fa4d7c701d3
c <machokoolkat@gmail.com>
2020-02-10 11:14:22 -0700

    If Statements, Rigidbodies, Scope, Components
    In class lecture, added double jump.

--- Project 1/Assets/Scripts/CharacterMovement.cs
+++ Project 1/Assets/Scripts/CharacterMovement.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     private Vector3 positionDirection;
     public float speed = 5f;
     public float gravity = -3f;
     public float jumpForce = 10f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
 
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
     
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
+        
         positionDirection.x = Input.GetAxis("Vertical")*speed;
-        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
+        
         positionDirection.y += gravity;
         controller.Move(positionDirection*Time.deltaTime);
     }
---
+++ Project 1/Assets/Scripts/RigidBodyForces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBodyForces : MonoBehaviour
+{
+   private Rigidbody rigidBodyObj;
+   public float force = 300;
+   private Vector3 forceVector;
+
+   private void Start()
+   {
+      rigidBodyObj = GetComponent<Rigidbody>();
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      forceVector.y = force;
+      rigidBodyObj.AddForce(forceVector);
+   }
+}
