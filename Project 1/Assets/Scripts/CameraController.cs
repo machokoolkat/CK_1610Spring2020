7000541c6472841b38cc1d5ee60d4c3006ba91cb
c <machokoolkat@gmail.com>
2020-02-12 11:21:59 -0700

    Class 2/12 added new components

---
+++ Project 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Camera))]
+
+public class CameraController : MonoBehaviour
+{
+    public Camera cameraObj;
+    void Start()
+    {
+        cameraObj = GetComponent<Camera>();
+    }
+    
+    void Update()
+    {
+        cameraObj.depth = 10f;
+    }
+}
---
+++ Project 1/Assets/Scripts/LightController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Light))]
+
+public class LightController : MonoBehaviour
+{
+    private Light lightObj;
+    void Start()
+    {
+        lightObj = GetComponent<Light>();
+        lightObj.intensity = 0f;
+    }
+    
+    void Update()
+    {
+        
+    }
+}
