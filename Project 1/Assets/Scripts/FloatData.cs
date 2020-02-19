4d900c86b9020b9bb14609c08b2c91da8d6eb800
c <machokoolkat@gmail.com>
2020-02-19 11:17:10 -0700

    Learned ScriptableObject
    Added health float and score int, health goes down on Cube Player by killer trigger

---
+++ Project 1/Assets/Scripts/FloatData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+
+    public void ChangeValue(float number)
+    {
+        value += number;
+    }
+}
---
+++ Project 1/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+    
+    
+}
