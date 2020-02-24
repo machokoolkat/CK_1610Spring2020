755c0ded7e358e873a6dca9e93137c73cb42256b
c <machokoolkat@gmail.com>
2020-02-23 17:34:25 -0700

    Color Behaviour Components
    Made color cube, changes color with key inputs

---
+++ Project 1/Assets/Scripts/ColorBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ColorBehaviour : MonoBehaviour
+{
+    //script from learn unity tutorial scripts as behaviour components
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
