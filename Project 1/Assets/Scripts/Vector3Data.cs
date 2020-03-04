5383db020c7ef09c5db0e983d7ecfcd71aed4bca
c <machokoolkat@gmail.com>
2020-03-04 11:14:13 -0700

    Checkpoints
    Created a simple checkpoint system with scriptable objects

---
+++ Project 1/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+
+    public void ChangePosition(Transform transformObj)
+    {
+        transformObj.position = value;
+    }
+
+    public void ChangeValueFromTransform(Transform transformObj)
+    {
+        value = transformObj.position;
+    }
+    
+    public void ChangeVector3Data(Vector3Data vector3DataObj)
+    {
+        vector3DataObj.value = value;
+    }
+}
