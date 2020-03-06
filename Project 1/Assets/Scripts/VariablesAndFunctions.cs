23bc00404ebdbb9fa43e8d4ffe352a23b091a3be
c <machokoolkat@gmail.com>
2020-03-05 21:15:59 -0700

    Variables and Functions
    Unity Learn Tutorial

---
+++ Project 1/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+// Code from unity learn scripting tutorials (variables and functions)
+public class VariablesAndFunctions : MonoBehaviour
+{
+    private int myInt = 5;
+
+    private void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
