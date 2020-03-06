b5169297cb8dde60340894fd1ccd81e3c9351e4a
c <machokoolkat@gmail.com>
2020-03-05 21:31:08 -0700

    If statements and syntax

---
+++ Project 1/Assets/Scripts/BasicSyntax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// code from unity learn tutorials, conventions and syntax
+public class BasicSyntax : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm About to hit the ground!");
+        }
+    }
+    /* apparently there can be multi line comments
+     * sick
+     */
+}
