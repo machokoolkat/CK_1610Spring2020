654f879df32988ef6bc9b86bb39d4d72b1b60bff
c <machokoolkat@gmail.com>
2020-03-08 00:34:01 -0600

    Update and FixedUpdate; Awake and Start
    unity learn

---
+++ Project 1/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+// code from learn unity tutorial on Awake and Start
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+    
+}
---
+++ Project 1/Assets/Scripts/UpdateAndFixedUpdate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// code from learn unity tutorial on Update and FixedUpdate
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate time:" + Time.deltaTime);
+    }
+
+    void Update()
+    {
+        Debug.Log("Update time:" + Time.deltaTime);
+    }
+}
