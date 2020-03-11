0ffba7d24a5d48547ec363c8f33f69c0a305bae6
c <machokoolkat@gmail.com>
2020-03-11 11:14:02 -0600

    In Class 3/11
    Lists and Loops

---
+++ Project 1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Collectable : ScriptableObject
+{
+    public int powerLevel = 10;
+    public Color collectableColor = Color.red;
+    public Sprite collectableSprite;
+}
---
+++ Project 1/Assets/Scripts/ListsAndLoops.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ListsAndLoops : ScriptableObject
+{
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+    
+    public void CheckList ()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj.Contains(singleString))
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+}
