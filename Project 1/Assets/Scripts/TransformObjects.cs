f5ed6593dbcfbd21b79cc2115a5f47ce6311870f
c <machokoolkat@gmail.com>
2020-03-10 22:47:43 -0600

    Transform Functions

---
+++ Project 1/Assets/Scripts/TransformObjects.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+//code from learn unity on Transform Functions
+public class TransformObjects : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+}
