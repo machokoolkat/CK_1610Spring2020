b1e59fc3eb6d1718f87cf77e887b97cf8c74c17b
c <machokoolkat@gmail.com>
2020-03-11 21:23:01 -0600

    Homework Weapons List Try

--- Project 1/Assets/Scripts/Collectable.cs
+++ Project 1/Assets/Scripts/Collectable.cs
@@ -6,4 +6,9 @@ public class Collectable : ScriptableObject
     public int powerLevel = 10;
     public Color collectableColor = Color.red;
     public Sprite collectableSprite;
+
+    public bool Contains(Collectable singleCollectable)
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- Project 1/Assets/Scripts/CollectableList.cs
+++ Project 1/Assets/Scripts/CollectableList.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class CollectableList : ScriptableObject
 {
     public List<Collectable> collectables;
-    
-    
+    public Collectable singleCollectable;
+
+    public void AddToList(Collectable collectableObj)
+    {
+        collectables.Add(collectableObj);
+    }
+
+    public void RemoveFromList(Collectable collectableObj)
+    {
+        for (int i = 0; i < collectables.Count; i++)
+        {
+            if (collectables[i] == collectableObj)
+            {
+                collectables.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void CheckList()
+    {
+        foreach (var obj in collectables)
+        {
+            if (obj.Contains(singleCollectable))
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
 }
