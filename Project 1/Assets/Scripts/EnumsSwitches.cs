9a4e8f6cdce5330813bdf59aa5110599c19b351d
c <machokoolkat@gmail.com>
2020-03-23 11:16:27 -0600

    Enums and Switches

---
+++ Project 1/Assets/Scripts/EnumsSwitches.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumsSwitches : MonoBehaviour
+{
+    public UnityEvent idleEvent, walkEvent, runEvent, jumpEvent;
+    public UnityEvent startingEvent, playingEvent, inStoreEvent, pausingEvent, endingEvent;
+    
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        InStore,
+        Pausing,
+        Ending
+    }
+
+    public GameStates currentGameState;
+    
+    public enum PlayerStates
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump
+    }
+
+    public PlayerStates currentPlayerState;
+
+    void Update()
+    {
+        switch (currentPlayerState)
+        {
+            case PlayerStates.Idle:
+                idleEvent.Invoke();
+                break;
+            case PlayerStates.Walk:
+                walkEvent.Invoke();
+                break;
+            case PlayerStates.Run:
+                runEvent.Invoke();
+                break;
+            case PlayerStates.Jump:
+                jumpEvent.Invoke();
+                break;
+            default:
+                print("I am any");
+                break;
+        }
+
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                startingEvent.Invoke();
+                break;
+            case GameStates.Playing:
+                playingEvent.Invoke();
+                break;
+            case GameStates.InStore:
+                inStoreEvent.Invoke();
+                break;
+            case GameStates.Pausing:
+                pausingEvent.Invoke();
+                break;
+            case GameStates.Ending:
+                endingEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
