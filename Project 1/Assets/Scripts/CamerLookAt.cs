36b97a1d9d5863b7de8200f26fa17c90a1f4e135
c <machokoolkat@gmail.com>
2020-04-03 23:31:25 -0600

    learn unity hw

---
+++ Project 1/Assets/Scripts/CamerLookAt.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+// script from learn unity on LookAt
+public class CamerLookAt : MonoBehaviour
+{
+    public Transform target;
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
---
+++ Project 1/Assets/Scripts/DestroyBasic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// script from learn unity on destroy
+public class DestroyBasic : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
