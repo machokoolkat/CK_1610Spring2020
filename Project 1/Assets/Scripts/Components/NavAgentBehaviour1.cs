febccf46f789841f7fc5c5ea0aadb84e10b11b51
c <machokoolkat@gmail.com>
2020-04-05 12:47:41 -0600

    new folders + new NavAgentBehaviour

---
+++ Project 1/Assets/Scripts/Components/NavAgentBehaviour1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class NavAgentBehaviour1 : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
