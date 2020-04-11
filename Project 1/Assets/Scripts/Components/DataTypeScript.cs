d193087e669c3fd19f2e6fb165c8c8bd68c97f2e
c <machokoolkat@gmail.com>
2020-04-10 23:30:25 -0600

    data types and delta time
    hw

---
+++ Project 1/Assets/Scripts/Components/DataTypeScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+// script from learn unity on data type
+public class DataTypeScript : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
+    }
+}
