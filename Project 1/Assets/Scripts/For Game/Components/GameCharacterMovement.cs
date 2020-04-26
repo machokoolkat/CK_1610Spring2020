4cbe019b751032fa9a618c603e9877d2392f0ca5
c <machokoolkat@gmail.com>
2020-04-26 15:55:15 -0600

    Final Game Project Setup
    New scene, reorganized folders, player movement script

---
+++ Project 1/Assets/Scripts/For Game/Components/GameCharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GameCharacterMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
+        
+        positionDirection.x = Input.GetAxis("Vertical")*speed;
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            positionDirection.y = jumpForce;
+            jumpCount++;
+        }
+        
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
