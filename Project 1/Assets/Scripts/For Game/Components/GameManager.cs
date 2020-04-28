6de021c07e51a3ce3c1b3c8a2f4c52d1f804a006
c <machokoolkat@gmail.com>
2020-04-27 21:14:13 -0600

    Player Collision

---
+++ Project 1/Assets/Scripts/For Game/Components/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void EndGame()
+    {
+        Debug.Log("GAME OVER");
+    }
+}
---
+++ Project 1/Assets/Scripts/For Game/Components/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// code from Brackeys
+public class PlayerCollision : MonoBehaviour
+{
+    public GameCharacterMovement movement;
+    
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Ground"))
+        {
+            movement.enabled = false;
+        }
+    }
+}
